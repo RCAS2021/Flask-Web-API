d32df786e319c206cf21b9283578d44c38cb33e1
Rafael Cruz <rcas2009@outlook.com>
2024-03-21 15:01:38 -0300

    Implementing URL variables

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def homepage():
     return "This is the homepage"
 
+@app.route('/<int:number>/')
+def incrementer(number):
+    return f"Incremented number = {str(number + 1)}"
+
+@app.route('/<string:name>/')
+def print_name(name):
+    return f"Your name is: {name}"
+
 if __name__ == "__main__":
     app.run()
